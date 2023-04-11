76fda8f662af9c99c5bfc65816f88b678d8b2c4b
mohamed.moustafa <mohamed.moustafa@dynatrace.com>
2023-04-11 11:59:14 +0200

    doing some enhacements on the FW

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -1,35 +1,49 @@
 package base;
 
 
+import dataReaders.loadPropertiesFiles;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class TestBase {
 
     public static WebDriver driver;
+    static String applicationURL = loadPropertiesFiles.URl.getProperty("URL");
+    static String browserType = loadPropertiesFiles.URl.getProperty("BrowserType");
 
+    public static ChromeOptions chromeOption() {
+        ChromeOptions options = new ChromeOptions();
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("profile.default.content_settings.popups", 0);
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+        options.addArguments("--start-maximized");
+        options.addArguments("--incognito");
+        return options;
+    }
     @BeforeClass
-    @Parameters({"browser"})
-    public void startDriver(@Optional("chrome") String browserName){
-        if(browserName.equalsIgnoreCase("chrome")){
+    public void startDriver(){
+        if(browserType.equalsIgnoreCase("chrome")){
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        }else if(browserName.equalsIgnoreCase("firefox")){
+            driver = new ChromeDriver(chromeOption());
+        }else if(browserType.equalsIgnoreCase("firefox")){
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
         }
-        driver.navigate().to("http://opencart.abstracta.us/index.php?route=common/home");
-        driver.manage().window().maximize();
+        driver.navigate().to(applicationURL);
     }
 
 
