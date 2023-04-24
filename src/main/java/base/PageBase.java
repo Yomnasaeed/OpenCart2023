de34dbfe097c58780795e2d02d7a3a0307e1d96d
yomna <yomna.saeed@hotmail.com>
2023-04-24 21:11:56 +0200

    added the invalid login in the RegressionSuite.xml

--- src/main/java/base/PageBase.java
+++ src/main/java/base/PageBase.java
@@ -52,6 +52,8 @@ public class PageBase {
         elementVisible(locator);
         return driver.findElement(locator).getText();
     }
+
+
     public boolean waitFor(ExpectedCondition<?> expectedCondition, Duration duration) {
         try {
             FluentWait<WebDriver> fluentWait = new FluentWait<>(driver)
